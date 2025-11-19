bccb9f3c81a58af9fc1ddd4db68af2eade1b31f6
Sagnik26Nik <88798706+Sagnik26Nik@users.noreply.github.com>
2025-11-18 21:13:19 -0500

    Update home.py

--- cosea_teachers_portal/home.py
+++ cosea_teachers_portal/home.py
@@ -696,5 +696,6 @@ def review_page():
     return render_template("review.html", schools=data["schools"])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
 
